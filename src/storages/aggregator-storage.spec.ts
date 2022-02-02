8733c2607414236064449dcb9e24b4f78186c7cb
Ilya Chistyakov <ichistyakovv@gmail.com>
2022-02-02 22:08:08 +0500

    refactor: improve aggregator storage tests

--- src/storages/aggregator-storage.spec.ts
+++ src/storages/aggregator-storage.spec.ts
@@ -1,64 +1,77 @@
 import { AggregatorStorage } from './aggregator-storage';
 import { CookieStorage } from './cookie-storage';
+import { Storage } from '../types/storage';
+
+const mockGet = jest.fn();
+const mockSet = jest.fn();
+const mockSync = jest.fn();
+const mockRemove = jest.fn();
+
+jest.mock('./cookie-storage', () => {
+    return {
+        CookieStorage: jest.fn().mockImplementation(() => {
+            return {
+                get: mockGet,
+                set: mockSet,
+                sync: mockSync,
+                remove: mockRemove,
+            };
+        }),
+    };
+});
 
 describe('Aggregator storage', () => {
-    let cookieStorage: CookieStorage;
+    const MockCookieStorage = CookieStorage as jest.MockedClass<
+        typeof CookieStorage
+    >;
+    const storageMock: Storage = new MockCookieStorage();
     const storageKey = 'strategy';
     const storageValue = 'local';
 
     beforeEach(() => {
-        cookieStorage = new CookieStorage();
+        mockSet.mockClear();
+        mockGet.mockClear();
+        mockSync.mockClear();
+        mockRemove.mockClear();
+        MockCookieStorage.mockClear();
     });
 
     it('Sets value', () => {
-        const storage = new AggregatorStorage([cookieStorage]);
-        const setCookieStorageSpy = jest.spyOn(cookieStorage, 'set');
+        const storage = new AggregatorStorage([storageMock]);
 
         storage.set(storageKey, storageValue);
 
-        expect(setCookieStorageSpy).toHaveBeenCalledWith(
-            storageKey,
-            storageValue,
-        );
+        expect(mockSet).toHaveBeenCalledWith(storageKey, storageValue);
     });
 
     it('Gets value', () => {
-        const storage = new AggregatorStorage([cookieStorage]);
-        const getCookieStorageSpy = jest.spyOn(cookieStorage, 'get');
+        const storage = new AggregatorStorage([storageMock]);
 
-        storage.set(storageKey, storageValue);
+        mockGet.mockImplementation((key) => storageValue);
 
-        // Act
         const value = storage.get(storageKey);
 
-        expect(getCookieStorageSpy).toHaveBeenCalledWith(storageKey);
+        expect(mockGet).toHaveBeenCalledWith(storageKey);
         expect(value).toBe(storageValue);
     });
 
     it('Syncs value', () => {
-        const storage = new AggregatorStorage([cookieStorage]);
-        const getCookieStorageSpy = jest.spyOn(cookieStorage, 'get');
-        const setCookieStorageSpy = jest.spyOn(cookieStorage, 'set');
+        const storage = new AggregatorStorage([storageMock]);
 
-        storage.set(storageKey, storageValue);
+        mockGet.mockImplementation((key) => storageValue);
 
-        // Act
         const value = storage.sync(storageKey);
 
-        expect(getCookieStorageSpy).toHaveBeenCalledWith(storageKey);
-        expect(setCookieStorageSpy).toHaveBeenCalledWith(
-            storageKey,
-            storageValue,
-        );
+        expect(mockGet).toHaveBeenCalledWith(storageKey);
+        expect(mockSet).toHaveBeenCalledWith(storageKey, storageValue);
         expect(value).toBe(storageValue);
     });
 
     it('Removes value', () => {
-        const storage = new AggregatorStorage([cookieStorage]);
-        const setCookieStorageSpy = jest.spyOn(cookieStorage, 'set');
+        const storage = new AggregatorStorage([storageMock]);
 
         storage.remove(storageKey);
 
-        expect(setCookieStorageSpy).toHaveBeenCalledWith(storageKey, false);
+        expect(mockSet).toHaveBeenCalledWith(storageKey, false);
     });
 });
