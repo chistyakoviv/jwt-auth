c071d024db9c73e54d6bc202e5ae1b657bf6e239
Ilya Chistyakov <ichistyakovv@gmail.com>
2022-02-03 21:27:52 +0500

    feat: add refresh controller mock

---
+++ src/http/refresh-controller.mock.ts
@@ -0,0 +1,9 @@
+import { RefreshController } from './refresh-controller';
+
+export const mockHandleRefresh = jest.fn();
+
+export const RefreshControllerMock = jest.fn().mockImplementation(() => {
+    return {
+        handleRefresh: mockHandleRefresh,
+    };
+});
