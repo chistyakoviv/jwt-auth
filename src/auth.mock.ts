116fca5ebbd62af9ec3ab6f6745e182c020636ea
Ilya Chistyakov <ichistyakovv@gmail.com>
2022-02-06 23:49:05 +0500

    refactor: replace auth with common mock

--- src/auth.mock.ts
+++ src/auth.mock.ts
@@ -1,3 +1,9 @@
+export * from './http/axios-adapter.mock';
 import { Auth } from './auth';
+import { AxiosAdapterMock } from './http/axios-adapter.mock';
 
-export const AuthMock = jest.fn() as jest.MockedClass<typeof Auth>;
+export const AuthMock = jest.fn().mockImplementation(() => {
+    return {
+        httpClient: new AxiosAdapterMock(),
+    };
+}) as jest.MockedClass<typeof Auth>;
--- src/strategies/local-strategy.mock.ts
+++ src/strategies/local-strategy.mock.ts
@@ -1,8 +1,9 @@
 import { LocalStrategy, DEFAULTS } from './local-strategy';
 import { RequestControllerMock } from '../http/request-controller.mock';
+import { Auth } from '../auth';
 
 export * from '../http/request-controller.mock';
-export { DEFAULTS } from './local-strategy';
+export { DEFAULTS, LocalStrategyOptions } from './local-strategy';
 export const mockInit = jest.fn();
 export const mockLogin = jest.fn();
 export const mockFetchUser = jest.fn();
@@ -11,19 +12,21 @@ export const mockCheck = jest.fn();
 export const mockLogout = jest.fn();
 export const mockAuth = jest.fn();
 
-export const LocalStrategyMock = jest.fn().mockImplementation((options) => {
-    const strategy: any = {
-        auth: mockAuth,
-        options: { ...DEFAULTS, ...options },
+export const LocalStrategyMock = jest
+    .fn()
+    .mockImplementation((auth: Auth, options) => {
+        const strategy: any = {
+            auth,
+            options: { ...DEFAULTS, ...options },
 
-        init: mockInit,
-        login: mockLogin,
-        fetchUser: mockFetchUser,
-        reset: mockReset,
-        check: mockCheck,
-        logout: mockLogout,
-    };
-    strategy.requestController = new RequestControllerMock(strategy);
+            init: mockInit,
+            login: mockLogin,
+            fetchUser: mockFetchUser,
+            reset: mockReset,
+            check: mockCheck,
+            logout: mockLogout,
+        };
+        strategy.requestController = new RequestControllerMock(strategy);
 
-    return strategy;
-}) as jest.MockedClass<typeof LocalStrategy>;
+        return strategy;
+    }) as jest.MockedClass<typeof LocalStrategy>;
