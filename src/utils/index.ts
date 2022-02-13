86d3df468da1e768494c88aecb09f74902345139
Ilya Chistyakov <ichistyakovv@gmail.com>
2022-02-13 13:20:44 +0500

    chore: cleanup

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -7,11 +7,11 @@ export function encodeValue(val: any): string {
     if (typeof val === 'string') {
         return val;
     }
+
     return JSON.stringify(val);
 }
 
 export function decodeValue(val: any): any {
-    // Try to parse as json
     if (typeof val === 'string') {
         try {
             return JSON.parse(val);
